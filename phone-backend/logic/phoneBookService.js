6b80bb02617ce4e1a874ead0c17c74661c545f5f
1n3ffbl3 <mizgier.m@gmail.com>
2019-02-12 09:32:02 +0100

    Adding PhoneBookService class

---
+++ phone-backend/logic/phoneBookService.js
@@ -0,0 +1,41 @@
+const phoneBookRepository = require('./phoneBookRepository');
+
+class PhoneBookService {
+
+    getAll() {
+        return phoneBookRepository.getAll()
+            .then(result => result.rows);
+    }
+
+    get(id) {
+        return phoneBookRepository.get(id)
+            .then(result => result.rows);
+    }
+
+    add(firstName, lastName, phoneNumber) {
+        const phoneBook = {
+            firstName: firstName,
+            lastName: lastName,
+            phoneNumber: phoneNumber
+        };
+        return phoneBookRepository.add(phoneBook)
+            .then(result => result.rowCount);
+    }
+
+    update(id, firstName, lastName, phoneNumber) {
+        return phoneBookRepository.get(id)
+            .then(result => {
+                const phoneBook = {
+                    id: id,
+                    firstName: firstName,
+                    lastName: lastName,
+                    phoneNumber: phoneNumber
+                };
+                return phoneBookRepository.update(phoneBook)
+                    .then(result => result.rowCount);
+            });
+    }
+}
+
+const phoneBookService = new PhoneBookService();
+module.exports = phoneBookService;
