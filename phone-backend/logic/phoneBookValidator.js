a9207419bdef4c0135ad276a3f5477e3c4c938cd
1n3ffbl3 <mizgier.m@gmail.com>
2019-02-12 09:36:06 +0100

    Using regex to validate phone number

---
+++ phone-backend/logic/phoneBookValidator.js
@@ -0,0 +1,12 @@
+const phoneNumberRegex = new RegExp('[+][0-9]{2} [0-9]{2} [0-9]{6,}');
+module.exports = {
+    validate: function(firstName, lastName, phoneNumber) {
+        if (!firstName || !lastName || !phoneNumber) {
+            return false;
+        }
+        return true;
+    },
+    validatePhoneNumber: function(phoneNumber) {
+        return phoneNumberRegex.test(phoneNumber);
+    }
+}
